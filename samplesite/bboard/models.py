7aeff548ef024d58ca8007ff5f29520c27492f59
Sanjar <sanzharkadyarliev@gmail.com>
2023-02-18 14:58:45 +0600

    upgraded admin

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name='Good')
+    content = models.TextField(null=True, blank=True, verbose_name='Description')
+    price = models.FloatField(null=True, blank=True, verbose_name='Price')
+    published = models.DateField(auto_now_add=True, db_index=True, verbose_name='Published date')
+
+class Meta:
+    verbose_name_plural = 'Announcement'
+    verbose_name = 'Announcement'
+    ordering = ['-published']
 
